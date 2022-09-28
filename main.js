06efc2c452da23d815efdde1fce106092c72b722
Agus9217 <justinibarrola92@gmail.com>
2022-09-28 16:38:27 -0300

    portfolio-web

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 const toogleBtn = document.getElementById('btn-menu') 
+const navBar = document.getElementById('navbar')
+const detailsBtn = document.getElementById('details')
 
 toogleBtn.addEventListener('click', () => {
-  toogleBtn.classList.toggle("change")
+  toogleBtn.classList.toggle("change") 
+  navBar.classList.toggle("show")
+})
+
+
+detailsBtn.addEventListener('click', (e) => {
+  console.dir(e)
 })
