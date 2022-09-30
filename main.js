d6bf492316981659f38528a1afcf364e9121e610
Agus9217 <justinibarrola92@gmail.com>
2022-09-30 11:07:12 -0300

    portfolio-web

--- main.js
+++ main.js
@@ -4,5 +4,20 @@ const navBar = document.getElementById('navbar')
 btnMenu.addEventListener('click', () => {
   btnMenu.classList.toggle('change')
   navBar.classList.toggle('show')
-
 })
+
+
+// window.onscroll = function() {scrollFunction()};
+
+// function scrollFunction() {
+//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//     document.getElementById("navbar").style.top = "0";
+//   } else {
+//     document.getElementById("navbar").style.top = "-50px";
+//   }
+// }
+
+window.onscroll = () => {
+  console.log(document.body.scrollTop)
+}
+
