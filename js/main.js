4765e2e1987bffc51c571eb863d31c1ba18d302a
Agus9217 <justinibarrola92@gmail.com>
2022-10-03 00:19:49 -0300

    portfolio-web

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const btnMenu = document.getElementById('btn-menu')
+const navBar = document.getElementById('navbar')
+
+btnMenu.addEventListener('click', () => {
+  btnMenu.classList.toggle('change')
+  navBar.classList.toggle('show')
+})
